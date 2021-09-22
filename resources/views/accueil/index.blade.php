1602a6ca848981d644f905edce8484f516886e55
julien <jujul_k@hotmail.com>
2021-09-22 18:22:25 +0200

    premier commit

---
+++ resources/views/accueil/index.blade.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108055482-1"></script>
+	    <script>
+	        window.dataLayer = window.dataLayer || [];
+	        function gtag(){dataLayer.push(arguments);}
+	        gtag('js', new Date());
+	        gtag('config', 'UA-108055482-1');
+	    </script>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Marine Robert, ostéopathe à Weyersheim près de Hoerdt dans le Bas-Rhin. Ostéopathie pour adulte, nourrisson et femmes enceintes.">
+		<meta name="author" content="Julien Kennel">
+
+		<title>Ostéopathe Marine Robert à Weyersheim. Ostéopathie pour adulte, nourrisson et femmes enceintes.</title>
+
+		<!-- Font Awesome Icons -->
+		<link href="{{asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+		<!-- Plugin CSS -->
+		<link href="{{asset('css/magnific-popup.css')}}" rel="stylesheet">
+
+		<!-- Theme CSS - Includes Bootstrap -->
+		<!-- <link href="{{asset('css/creative-v'.config('app.version').'.css')}}" rel="stylesheet"> -->
+		<link href="{{asset('css/creative.css')}}" rel="stylesheet">
+
+		<!-- <link rel="shortcut icon" type="image/png" href="{{asset('images/logo.ico')}}" /> -->
+
+		<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+		<script type="text/javascript" src="{{asset('tarteaucitron/tarteaucitron.js')}}"></script>
+
+        <script type="text/javascript">
+        tarteaucitron.init({
+    	  "privacyUrl": "", /* Privacy policy url */
+
+    	  "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+    	  "cookieName": "tarteaucitron", /* Cookie name */
+    
+    	  "orientation": "middle", /* Banner position (top - bottom) */
+    	  "showAlertSmall": true, /* Show the small banner on bottom right */
+    	  "cookieslist": true, /* Show the cookie list */
+
+    	  "adblocker": false, /* Show a Warning if an adblocker is detected */
+    	  "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+    	  "highPrivacy": true, /* Disable auto consent */
+    	  "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+
+    	  "removeCredit": false, /* Remove credit link */
+    	  "moreInfoLink": true, /* Show more info link */
+    	  "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+
+    	  //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+                          
+    	  "readmoreLink": "/cookiespolicy" /* Change the default readmore link */
+        });
+        </script>
+
+	</head>
+
+	<body id="page-top">
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+			<div class="container">
+				<a class="navbar-brand js-scroll-trigger" href="#page-top">Marine Robert</a>
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto my-2 my-lg-0">
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#about">Accueil</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#perinatalite">Périnatalité</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#sportif">Sportif</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#honoraires">Honoraires</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<!-- Masthead -->
+		<header class="masthead">
+			<div class="container h-100">
+				<div class="row h-100 align-items-center justify-content-center text-center">
+					<div class="col-lg-10 align-self-end">
+						<h1 class="text-uppercase text-white font-weight-bold">MARINE ROBERT,<br>OSTÉOPATHE À WEYERSHEIM</h1>
+						<hr class="divider light my-4">
+					</div>
+					<div class="col-lg-8 align-self-end">
+						<p class="text-white-75 font-weight-light mb-5">"Notre corps est la barque qui nous portera jusqu’à l’autre rive de l’océan de la vie. Il faut en prendre soin."<br>Swami Vivekananda</p>
+						<!-- <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">En savoir plus</a> -->
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<!-- About Section -->
+		<section class="page-section bg-primary" id="about">
+			<div class="container">
+				<div class="row justify-content-center" style="overflow: hidden;">
+					<div class="col-lg-8 text-center">
+						<h2 class="text-white mt-0">Accueil</h2>
+					</div>
+					<div class="col-lg-12 align-middle">
+						<hr class="divider light my-4">
+						<div class="row text-white mb-4" data-aos="fade-right" data-aos-duration="800">
+							<img class="img-fluid float-left col-md-4 col-xs-12 mb-4" src="{{asset('img/dos.jpg')}}" alt="" style="border-radius: 1em;" >
+							<div class="col-md-8 col-xs-12 p-4" style="margin-top: auto;margin-bottom: auto;">
+								<h6>L’ostéopathie : qu’est-ce que c’est ?</h6>
+								<p class="mb-2 text-white-50 text-justify">
+									L’ostéopathie est une thérapie manuelle qui aide le corps du patient à retrouver son équilibre et son fonctionnement optimal. Toute perte de mobilité des articulations, des muscles, des ligaments ou des viscères peut provoquer un déséquilibre de l’état de santé...
+								</p>
+								<button class="btn btn-light bouton_info" data-info="L’ostéopathie : qu’est-ce que c’est ?">En savoir plus</button>
+							</div>
+						</div>
+
+						<div class="row text-white mt-4" data-aos="fade-left" data-aos-duration="800">
+							<div class="col-md-6 col-xs-12 p-4">
+								<h6>Séance d’ostéopathie : le déroulement</h6>
+								<p class="mb-2 text-white-50 text-justify">
+									Une séance commence par une anamnèse complète (interrogatoire par rapport au motif de consultation, antécédents), l’ostéopathe examine et évalue la mobilité de différentes zones de votre corps afin de vérifier si vous pouvez être pris en charge en ostéopathie ou si vous devez être...
+								</p>
+								<button class="btn btn-light bouton_info" data-info="Séance d’ostéopathie : le déroulement">En savoir plus</button>
+							</div>
+							<div class="col-md-6 col-xs-12 p-4 text-right">
+								<h6>Champs d’applications :</h6>
+								<p class="mb-2 text-white-50 text-justify">
+									Peu importe que vous soyez jeune ou âgé, sportif amateur ou de haut niveau, il est souhaitable de faire appel à un ostéopathe de façon régulière. En effet, la prévention joue un rôle majeur dans la santé. Lors de chaque séance l’ostéopathe va vérifier l’ensemble du corps afin de...
+								</p>
+								<button class="btn btn-light float-right bouton_info" data-info="Champs d’applications">En savoir plus</button>
+							</div>
+						</div>
+						<!-- <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a> -->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- About Section -->
+		<section class="page-section" id="perinatalite">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-8 text-center">
+						<h2 class="text-dark mt-0">Périnatalité</h2>
+					</div>
+					<div class="col-lg-12 align-middle position-relative" data-aos="fade-up" data-aos-duration="800">
+						<hr class="divider my-4">
+						<div class="row text-dark mb-4" id="bloc-perinatalite">
+							<div class="col-md-8 col-xs-12">
+								<div class="col-md-12 col-xs-12">
+									<h6>Femme enceinte</h6>
+									<p class="mb-2 text-dark-50 text-justify">
+										Pendant la grossesse, il peut arriver que la femme subisse des maux divers, surtout au niveau du dos (lombalgies, sciatalgie). Si vous vous rapprochez de la fin de votre grossesse, vous êtes sans doute dans ce cas. L’ostéopathe peut soulager vos tensions...
+									</p>
+									<button class="btn btn-primary bouton_info" data-info="Femme enceinte">En savoir plus</button>
+								</div>
+								<div class="col-md-12 col-xs-12" id="bloc-nourisson">
+									<h6 class="text-right">Nourrisson</h6>
+									<p class="mb-2 text-dark-50 text-justify">
+										Le nourrisson a potentiellement eu des chocs, voire des contraintes dus à l’accouchement et la grossesse. L’ostéopathie est conseillée en prévention chez le nouveau-né. En premier lieu pour établir le bilan de la santé de l'enfant. Césarienne, accouchement avec des instruments, péridurale...</p>
+									<button class="btn btn-primary float-right bouton_info" data-info="Nourrisson">En savoir plus</button>
+								</div>
+							</div>
+							<img class="img-fluid col-md-4 col-xs-12" src="{{asset('img/femme_enceinte.jpg')}}" alt="" style="border-radius: 1em;" id="image-perinatalite">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		<!-- About Section -->
+		<section class="page-section bg-dark" id="sportif">
+			<div class="container">
+				<div class="row justify-content-center" style="overflow: hidden;">
+					<div class="col-lg-8 text-center">
+						<h2 class="text-white mt-0">Sportif</h2>
+					</div>
+					<div class="col-lg-12 align-middle" data-aos="fade-left" data-aos-duration="800">
+						<hr class="divider light my-4">
+						<div class="row text-white mb-4">
+							<img class="img-fluid float-left col-md-4 col-xs-12 mb-4" src="{{asset('img/sportif.jpg')}}" alt="" style="border-radius: 1em;" >
+							<div class="col-md-8 col-xs-12 pl-4 pr-4">
+								<h6>Ostéopathie et sport</h6>
+								<p class="mb-2 text-white-50 text-justify">
+									En fonction des disciplines sportives, certains muscles travaillent plus que d'autres. Ils se retrouvent logiquement fragilisés ; le risque de blessure est plus important. Traiter, soulager et prévenir les douleurs, les compétences d'un ostéopathe sont multiples...
+								</p>
+								<button class="btn btn-light bouton_info" data-info="Ostéopathie et sport">En savoir plus</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="page-section bg-primary" id="honoraires">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-8 text-center">
+						<h2 class="text-white mt-0">Honoraires</h2>
+					</div>
+					<div class="col-lg-12  align-middle">
+						<hr class="divider light my-4">
+						<div class="row text-white mb-4" data-aos="fade-right" data-aos-duration="800">
+							<img class="img-fluid float-left col-md-4 col-xs-12 mb-4" src="{{asset('img/accueil.jpg')}}" alt="" style="border-radius: 1em;" >
+							<div class="col-md-8 col-xs-12 p-4" style="margin-top: auto;margin-bottom: auto;">
+								<!-- <h6>L’ostéopathie : qu’est-ce que c’est ?</h6> -->
+								
+								<p class="mb-2 text-white-50 text-justify">Les honoraires d’une séance d’ostéopathie au cabinet sont de 50 Euros.</p>
+
+								<p class="mb-2 text-white-50 text-justify">J’accepte le règlement par chèque ainsi qu'en espèces, les cartes bancaires ne sont pas acceptées.</p>
+
+								<p class="mb-2 text-white-50 text-justify">Bien que l'Ostéopathie soit reconnue par l'Etat comme profession, les séances ne sont pas prises en charge par la Sécurité Sociale mais peuvent l'être en totalité ou en partie par votre Mutuelle Complémentaire suivant votre contrat.</p>
+
+								<p class="mb-2 text-white-50 text-justify">Une note d’honoraire vous sera remise à cet effet en fin de consultation. Vous n’aurez plus qu’à transmettre celle-ci à votre mutuelle qui pourra ensuite vous rembourser.</p>
+
+								<p class="mb-2 text-white-50 text-justify">Pour savoir si votre Mutuelle Complémentaire prend en charge l'ostéopathie, n'hésitez pas à la contacter directement.</p>
+								
+								<!-- <button class="btn btn-light" href="#services">En savoir plus</button> -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		<!-- Contact Section -->
+		<section class="page-section text-white" id="contact">
+			<div class="container">
+				<div class="row justify-content-center text-dark pb-4">
+					<div class="col-md-8 col-xs-12 text-center">
+						<h2 class="mt-0">Contact</h2>
+						<hr class="divider my-4">
+					</div>
+					<div class="row text-center col-12">
+						<div class="col-md-4 col-xs-12 ml-auto text-center mb-5 mb-lg-0">
+							<i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+							<div>
+								<a href="tel:+33659990873" class="text-primary" style="font-size: 1.2rem;">06.59.99.08.73</a>
+							</div>
+						</div>
+						<div class="col-md-4 col-xs-12 ml-auto text-center mb-5 mb-lg-0">
+							<i class="fas fa-home fa-3x mb-3 text-muted"></i>
+							<div class="text-primary" style="font-size: 1.2rem;">
+								1 Rue Saint Michel 67720 Weyersheim
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-xs-12 mr-auto text-center">
+							<i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+							<a class="d-block text-primary" style="font-size: 1.2rem;" href="mailto:osteopathe.marine@gmail.com">osteopathe.marine@gmail.com</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="row justify-content-center mt-4">
+					<div class="col-md-5 col-xs-12 text-center">
+						<p class="text-muted">
+							Consultations sur rendez-vous uniquement.
+						</p>
+						<p class="text-muted mb-5">
+							Pour plus de renseignement n’hésitez pas à me contacter par mail ou par téléphone.
+						</p>
+					</div>
+				</div>
+
+				<div class="col-xs-6 col-md-12 text-center">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2632.57891252545!2d7.799289415222751!3d48.713528918850336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4796c121636221a7%3A0xdb3e8e5a8432b2ea!2s1%20Rue%20Saint-Michel%2C%2067720%20Weyersheim!5e0!3m2!1sfr!2sfr!4v1585219789730!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" height="400" width="100%"></iframe>
+				</div>
+			</div>
+		</section>
+
+		<!-- Footer -->
+		<footer class="bg-dark py-5">
+			<div class="container">
+				<div class="small text-center text-muted">Copyright &copy; {{date('Y')}} - Marine Robert</div>
+			</div>
+		</footer>
+
+		@include('accueil/modal')
+
+		<!-- Bootstrap core JavaScript -->
+		<script src="{{asset('js/jquery.min.js')}}"></script>
+		<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+
+		<!-- Plugin JavaScript -->
+		<script src="{{asset('js/jquery.easing.min.js')}}"></script>
+		<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+
+		<!-- Custom scripts for this template -->
+		<!-- <script src="{{asset('js/creative-v'.config('app.version').'.js')}}"></script> -->
+		<script src="{{asset('js/creative.js')}}"></script>
+
+		<script>
+		 	AOS.init();
+		</script>
+
+		<!-- <script src="{{asset('js/perso-v'.config('app.version').'.js')}}"></script> -->
+		<script src="{{asset('js/perso.js')}}"></script>
+
+		<script type="text/javascript">
+	        tarteaucitron.user.gtagUa = 'UA-108055482-1';
+	        tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
+	        (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+        </script>
+
+
+	</body>
+</html>
---
+++ resources/views/accueil/modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="modal_info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="margin-top: 10vh; padding-bottom: 100px;">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content modal-fluid ">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
+			</div>
+		</div>
+	</div>
+</div>
